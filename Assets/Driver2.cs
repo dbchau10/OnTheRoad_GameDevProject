2d6b880927f6d04781034758513ea4370e3dbff9
Minh Bao <bebaoboy@gmail.com>
2023-11-18 00:17:33 +0700

    minor changes

--- Assets/Driver2.cs
+++ Assets/Driver2.cs
@@ -29,30 +29,29 @@ public class Driver2 : MonoBehaviour
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         //transform.Rotate(0, 0, -steerAmount);
+        //transform.Translate(0,moveAmount, 0);
+
         player.angularVelocity += -(Input.GetAxis("Horizontal") * steerSpeed);
-        // player.AddTorque(Input.GetAxis("Horizontal") * -1);
         if ((Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow)) && isMoving)
         {
             isMoving = false;
             ps.Stop();
-            Debug.Log("Stop fuming");
+            //Debug.Log("Stop fuming");
         }
         if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
         {
             player.angularVelocity = 0;
         }
-        //transform.Translate(0,moveAmount, 0);
         if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)) && !isMoving) isMoving = true;
         if (isMoving)
         {
             direction = Mathf.Sign(Vector2.Dot(player.velocity, player.GetRelativeVector(Vector2.up)));
-            //player.rotation += -Input.GetAxis("Horizontal") * Input.GetAxis("Vertical") * moveSpeed * player.velocity.magnitude * direction;
             player.AddRelativeForce(Vector2.up * Input.GetAxis("Vertical") * moveSpeed);
             player.AddRelativeForce(Vector2.right * player.velocity.magnitude * Input.GetAxis("Horizontal") / 2);
             if (!ps.isEmitting)
             {
                 ps.Play();
-                Debug.Log("Emit FUmes" + Time.deltaTime);
+                //Debug.Log("Emit FUmes" + Time.deltaTime);
             }
         }
     }
