b2c205ce8319b48dffbc94c733b49b9ca82f8b4d
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2023-12-18 03:13:37 +0700

    check collider for player meet traffic light

--- Assets/Script/PlayerMeetTrafficLight.cs
+++ Assets/Script/PlayerMeetTrafficLight.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerMeetTrafficLight : MonoBehaviour
 {
    
+   public GameObject TrafficLight;
    void OnTriggerEnter2D(Collider2D other)
    {
-       
+        if (TrafficLight.GetComponent<ChangeTrafficLightState>().currentLightState == "Red"){
+            if (other.tag == "HeadCar"){
+                FindObjectOfType<QuizManager>().QuizOpen();
+            }
+        }
    }
 }
