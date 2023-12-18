63a77b2a95f90657af5b287568608830aa78c12b
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2023-12-19 00:40:45 +0700

    update warning timer violation

--- Assets/Script/PlayerMeetTrafficLight.cs
+++ Assets/Script/PlayerMeetTrafficLight.cs
@@ -6,7 +6,7 @@ public class PlayerMeetTrafficLight : MonoBehaviour
 {
    
    public GameObject TrafficLight;
-
+   public GameObject WarningTimer;
    public bool checkOpenQuiz=false;
 
 //    public bool checkSkip = false;
@@ -16,6 +16,26 @@ public class PlayerMeetTrafficLight : MonoBehaviour
             if (other.tag == "HeadCar"){
                 FindObjectOfType<QuizManager>().QuizOpen();
                 checkOpenQuiz = true;
+            }
+        }
+       
+   }
+
+ 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+         if (other.tag == "HeadCar"){
+                WarningTimer.GetComponent<WarningTimer>().StopWarning();
+               
+            }
+    }
+  
+   void OnTriggerExit2D(Collider2D other)
+   {
+        if (TrafficLight.GetComponent<ChangeTrafficLightState>().currentLightState == "Red" ){
+            if (other.tag == "HeadCar"){
+                WarningTimer.GetComponent<WarningTimer>().Warning();
+               
             }
         }
    }
