7155ff5a39bedb0df7b51191ec5bfd427b21ad97
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2024-01-03 20:05:45 +0700

    update thing in map level 3

--- Assets/Script/FollowCamera.cs
+++ Assets/Script/FollowCamera.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FollowCamera : MonoBehaviour
 {
 
     // this camera's position should be at the same as the car's position
    [SerializeField] GameObject thingToFollow;
+
+   Scene currentScene;
+ 
+   void Start()
+   {
+    currentScene = SceneManager.GetActiveScene();
+       
+   }
     void LateUpdate()
     {
           var targetPosition = thingToFollow.transform.position + new Vector3(0,0,-10);
+
+            if (currentScene.name == "SampleMap"){
           transform.position = new Vector3(Mathf.Clamp(targetPosition.x,-27.4f,356f), Mathf.Clamp(targetPosition.y,-0.5f,158f), targetPosition.z);
+            }
+            else if (currentScene.name == "SampleMapLevel2"){
+                
+            }
+            else if (currentScene.name == "SampleMapLevel3"){
+                  transform.position = new Vector3(Mathf.Clamp(targetPosition.x,5.7f,389.9f), Mathf.Clamp(targetPosition.y,-92f,127.1f), targetPosition.z);
+            }
     }
 }
